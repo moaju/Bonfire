2b8ba059a07ef33e7f44b3e26747616e85a105cf
양지현 <jhnyang@khu.ac.kr>
2019-01-26 15:12:32 +0900

    basic with dependency

---
+++ bonfireSql/src/main/java/com/bonfire/bonfireSql/BonfireSqlApplication.java
@@ -0,0 +1,14 @@
+package com.bonfire.bonfireSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonfireSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonfireSqlApplication.class, args);
+	}
+
+}
+
